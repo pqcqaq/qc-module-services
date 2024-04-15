71e681df90506225378815a1f67a4399133ef71e
pqcqaq <905739777@qq.com>
2024-04-15 23:57:07 +0800

    自动建表功能

--- attachments/src/main/java/online/zust/qcqcqc/services/module/attachment/entity/Attachment.java
+++ attachments/src/main/java/online/zust/qcqcqc/services/module/attachment/entity/Attachment.java
@@ -1,32 +1,32 @@
 package online.zust.qcqcqc.services.module.attachment.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import online.zust.qcqcqc.utils.entity.BaseEntity;
+import online.zust.qcqcqc.utils.generators.annotation.ColumnType;
+import online.zust.qcqcqc.utils.generators.enums.DataType;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * @author pqcmm
  */
+@EqualsAndHashCode(callSuper = true)
 @TableName("attachment")
 @Data
-public class Attachment implements Serializable {
+public class Attachment extends BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 7519503360098918409L;
-    @TableId
-    private Long id;
+    @ColumnType(type = DataType.Varchar, comment = "文件名", nullable = false)
     private String originalFilename;
+    @ColumnType(type = DataType.Varchar, comment = "文件路径", nullable = false)
     private String url;
+    @ColumnType(type = DataType.Varchar, length = 32, comment = "文件md5", nullable = false)
     private String md5;
+    @ColumnType(type = DataType.Varchar, length = 31, comment = "文件类型", nullable = false)
     private String fileType;
+    @ColumnType(type = DataType.Varchar, length = 127, comment = "文件标签", nullable = false)
     private String tag;
-    @TableField(fill = FieldFill.INSERT)
-    private Long createBy;
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-    @TableLogic
-    @TableField(fill = FieldFill.INSERT)
-    private Boolean deleted;
 }
--- operator-logs/src/main/java/online/zust/qcqcqc/services/module/log/entity/OperatorLog.java
+++ operator-logs/src/main/java/online/zust/qcqcqc/services/module/log/entity/OperatorLog.java
@@ -1,29 +1,32 @@
 package online.zust.qcqcqc.services.module.log.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import online.zust.qcqcqc.services.module.log.enums.LogLevel;
+import online.zust.qcqcqc.utils.entity.BaseEntity;
+import online.zust.qcqcqc.utils.generators.annotation.ColumnType;
+import online.zust.qcqcqc.utils.generators.enums.DataType;
 
-import java.util.Date;
+import java.io.Serial;
 
 /**
  * @author qcqcqc
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @TableName(value = "operator_log")
-public class OperatorLog {
-    @TableId
-    private Long id;
+public class OperatorLog extends BaseEntity {
+    @Serial
+    private static final long serialVersionUID = 8008114011853263091L;
+    @ColumnType(type = DataType.Varchar, length = 31, comment = "日志级别", defaultValue = "'INFO'", nullable = false)
     private LogLevel level;
+    @ColumnType(type = DataType.Varchar, length = 1023, comment = "日志信息", nullable = false)
     private String msg;
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-    @TableField(fill = FieldFill.INSERT)
-    private Long createBy;
+    @ColumnType(type = DataType.Tinyint, length = 1, comment = "是否成功", nullable = false)
     private Boolean success;
+    @ColumnType(type = DataType.Varchar, length = 1023, comment = "失败原因")
     private String cause;
+    @ColumnType(type = DataType.Varchar, length = 2047, comment = "元数据", nullable = false)
     private String metadata;
 }
